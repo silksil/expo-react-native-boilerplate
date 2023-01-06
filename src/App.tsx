4abf0733be0d54cccf019d348d353c4df83addfc
Sil <silkreulen@gmail.com>
2023-01-06 14:48:19 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar'
+import { Text } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import useCachedResources from './hooks/useCachedResources'
